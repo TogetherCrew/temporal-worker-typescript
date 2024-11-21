d1fd40c66470b96d85fc8592dfc0692455b4e84c
Cyrille Derche <c.derche@me.com>
2024-11-21 18:23:00 +0100

    bottleneck for s3

--- src/client.ts
+++ src/client.ts
@@ -2,11 +2,12 @@ import { Connection, Client } from '@temporalio/client';
 import { DiscourseExtractWorkflow } from './workflows';
 import { QUEUE } from './shared/queues';
 
-const endpoint = 'community.singularitynet.io';
+const endpoint = 'community.freetrade.io'
+// const endpoint = 'community.singularitynet.io';
 // const endpoint = 'gov.optimism.io';
 
 async function run() {
-  const connection = await Connection.connect({ address: 'temporal:7233' });
+  const connection = await Connection.connect({ address: 'localhost:7233' });
 
   const client = new Client({
     connection,
