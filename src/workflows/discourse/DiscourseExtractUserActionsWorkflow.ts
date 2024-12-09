7a6deaf59b4425c2b05dcc33b93a1970b000815c
Cyrille <1618640+cyri113@users.noreply.github.com>
2024-12-09 19:28:14 +0100

    extend fetch actions to 60m

--- src/workflows/discourse/DiscourseExtractUserActionsWorkflow.ts
+++ src/workflows/discourse/DiscourseExtractUserActionsWorkflow.ts
@@ -4,7 +4,7 @@ import pLimit from 'p-limit';
 const MAX_PARTITIONS = 1000;
 
 const { fetchActionsToS3 } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '10m',
+  startToCloseTimeout: '60m',
   retry: {
     maximumAttempts: 3,
   },
