6a04b7ec989db0babf1ed453c6761bc87c5a4b7e
cyri113 <c.derche@me.com>
2025-03-13 17:43:26 +0100

    export TelegramGetCommunityWorkflow

--- src/workflows/telegram/index.ts
+++ src/workflows/telegram/index.ts
@@ -1,3 +1,4 @@
 export { TelegramVerifyWorkflow } from './TelegramVerifyWorkflow';
 export { TelegramEventWorkflow } from './TelegramEventWorkflow';
 export { TelegramQuestionWorkflow } from './TelegramQuestionWorkflow';
+export { TelegramGetCommunityWorkflow } from './TelegramGetCommunityWorkflow';
