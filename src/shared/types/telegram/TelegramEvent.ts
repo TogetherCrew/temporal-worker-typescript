7690674770aaf1f2dee0587df276a6fb234ccbd9
cyri113 <c.derche@me.com>
2025-04-11 11:54:26 +0200

    fix migrate to chat id

--- src/shared/types/telegram/TelegramEvent.ts
+++ src/shared/types/telegram/TelegramEvent.ts
@@ -3,5 +3,5 @@ export enum TelegramEvent {
   MESSAGE_REACTION = 'message_reaction',
   EDITED_MESSAGE = 'edited_message',
   MESSAGE = 'message',
-  MIGRATE_TO_CHAT_ID = 'message.migrate_to_chat_id',
+  MIGRATE_TO_CHAT_ID = 'message:migrate_to_chat_id',
 }
