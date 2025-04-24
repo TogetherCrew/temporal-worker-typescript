3c4e470204f79b760db379a52e42e0c018d9f2a1
cyri113 <c.derche@me.com>
2025-04-24 12:16:28 +0200

    add to workflow index

--- src/workflows/telegram/index.ts
+++ src/workflows/telegram/index.ts
@@ -2,3 +2,4 @@ export { TelegramVerifyWorkflow } from './TelegramVerifyWorkflow';
 export { TelegramEventWorkflow } from './TelegramEventWorkflow';
 export { TelegramQuestionWorkflow } from './TelegramQuestionWorkflow';
 export { TelegramGetCommunityWorkflow } from './TelegramGetCommunityWorkflow';
+export { TelegramGetPlatformWorkflow } from './TelegramGetPlatformWorkflow';
