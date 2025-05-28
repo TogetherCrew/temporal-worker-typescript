160c8219eab3ad4d7632ba9c56aca6a73cab5455
cyri113 <c.derche@me.com>
2025-05-28 11:32:36 +0200

    feat: add TelegramVectorIngestionWorkflow

--- src/workflows/telegram/index.ts
+++ src/workflows/telegram/index.ts
@@ -2,3 +2,4 @@ export { TelegramVerifyWorkflow } from './TelegramVerifyWorkflow';
 export { TelegramEventWorkflow } from './TelegramEventWorkflow';
 export { TelegramQuestionWorkflow } from './TelegramQuestionWorkflow';
 export { TelegramSummaryWorkflow } from './TelegramSummaryWorkflow';
+export { TelegramVectorIngestionWorkflow } from './TelegramVectorIngestionWorkflow';
