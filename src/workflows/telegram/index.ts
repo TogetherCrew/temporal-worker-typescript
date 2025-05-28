1a3781d95f7a94e282f82b522b0e6d52b7d30033
cyri113 <c.derche@me.com>
2025-05-28 10:16:08 +0200

    refactor: clean up Telegram workflow exports by removing unused workflows

--- src/workflows/telegram/index.ts
+++ src/workflows/telegram/index.ts
@@ -1,5 +1,3 @@
 export { TelegramVerifyWorkflow } from './TelegramVerifyWorkflow';
 export { TelegramEventWorkflow } from './TelegramEventWorkflow';
 export { TelegramQuestionWorkflow } from './TelegramQuestionWorkflow';
-export { TelegramGetCommunityWorkflow } from './TelegramGetCommunityWorkflow';
-export { TelegramGetPlatformWorkflow } from './TelegramGetPlatformWorkflow';
