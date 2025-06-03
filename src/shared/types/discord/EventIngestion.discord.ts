6da2b32bf62dd2747b98ac8ee0aaf244e664e801
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-06-03 14:12:49 +0200

    style: format the code

--- src/shared/types/discord/EventIngestion.discord.ts
+++
@@ -1,8 +0,0 @@
-import { DiscordEventType, EventPayloadMap } from './DiscordEvents';
-export interface GatewayEventInput<
-  T extends DiscordEventType = DiscordEventType,
-> {
-  type: T;
-  guildId: string;
-  payload: EventPayloadMap[T];
-}
